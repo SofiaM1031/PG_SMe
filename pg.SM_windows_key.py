8477b77b35c209bbe25e39e3c0d65b8e28590069
SofiaM1031 <33524284+SofiaM1031@users.noreply.github.com>
2018-01-11 12:57:32 -0500

    Add files via upload

--- pg.SM_windows_key.py
+++ pg.SM_windows_key.py
@@ -13,11 +13,10 @@ pg.hotkey('winleft')
 pg.typewrite('chrome\n',.3)
 
 pg.hotkey('winleft', 'up')
-pg.typewrite('gmail new account\n',.1)
+pg.hotkey('winleft', 'up')
+pg.hotkey('winleft', 'up')
+pg.typewrite('https://accounts.google.com/SignUp?hl=en\n',.1)
 
-
-time.sleep(2)
-pg.click(420, 290)
 
 time.sleep(2)
 pg.hotkey('tab')
@@ -27,44 +26,59 @@ pg.hotkey('tab')
 time.sleep(2)
 
 counter=0
-while counter < 6:
+while counter < 15:
     pg.typewrite(symbols[random.randint(1,len(name)-1)])
     counter += 1
 
 #Last
+time.sleep(2)
+
 pg.hotkey('tab')
 
 counter=0
-while counter < 6:
+while counter < 15:
     pg.typewrite(symbols[random.randint(1,len(name)-1)])
     counter += 1
 
+
 #Username
+time.sleep(2)
+
 pg.hotkey('tab')
 
 counter=0
-while counter < 6:
+while counter < 15:
     pg.typewrite(symbols[random.randint(1,len(name)-1)])
     counter += 1
 
 #Password
+time.sleep(2)
+    
 pg.hotkey('tab')
 pg.hotkey('tab')
 counter=0
-while counter < 6:
-    pg.typewrite(symbols[random.randint(1,len(name)-1)])
+pw=''
+while counter < 15:
+    a = symbols[random.randint(1,len(name)-1)]
+
+    pg.typewrite(a)
+    pw=pw+a
+
     counter += 1
+
+#password saved as A
 
 #Confirm Password
 time.sleep(2)
-pg.hotkey('shift', 'up')
-
-time.sleep(2)
-pg.hotkey('ctrl','c')
-
 pg.hotkey('tab')
 
+time.sleep(2)
+pg.typewrite(pw)
 
-pg.hotkey('ctrl', 'v')
 
+#Month
+time.sleep(2)
 
+pg.hotkey('tab')
+pg.hotkey('down')
+pg.hotkey('enter')
